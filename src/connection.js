3ecb92064fbebddd07c533971bd691735edfe1bd
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2022-05-19 17:40:55 -0400

    comentarios

--- src/connection.js
+++ src/connection.js
@@ -5,23 +5,31 @@ const isDev = require('electron-is-dev');
 
 let sequelize;
 
-if(!isDev) {
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join( appdata('inventario_maquinaria'), 'm.data'),
-    logging: false
-  });
-}else{
+// configuracion en modo desarrollo
+if(isDev) {
+
   sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: "m.data",
     logging: false
   });
+
+  //configuracion en produccion
+}else{
+
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join( appdata('inventario_maquinaria'), 'm.data'),
+    logging: false
+  });
+
 }
 
-
+/**
+ * Esta funcion asincrona crea cada una de las tablas de la base de datos
+ */
 (async () => {
-	await sequelize.sync({alter: true}); 
+	await sequelize.sync({alter: true});  
 })();
 
 module.exports = sequelize;
