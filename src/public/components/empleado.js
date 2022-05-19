bbfa03850f9b6591f6360e69e82c9b22f8e52f3c
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2022-05-19 19:21:12 -0400

    vista de prueba

---
+++ src/public/components/empleado.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// componente home
+let empleado = Vue.component('empleado', {
+
+    data: function() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        
+    },
+
+
+	template: `
+
+      <h1 style="border:41px solid;">Vista Empleado</h1>
+    `
+});
+
+export default empleado;
